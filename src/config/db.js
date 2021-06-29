2f1a42be907cf1408eb1847f55d5a1e94b64e1f5
Augusto Otto Molke <augustomolke@gmail.com>
2021-06-29 12:32:32 -0300

    improvement on db config

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DB_CONNECT, {
   useNewUrlParser: true,
+  useCreateIndex: true,
 });
-/* remove alerts from console*/
-mongoose.set("useCreateIndex", true);
+
 module.exports = mongoose.connection;
